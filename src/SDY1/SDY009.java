e7a968fb88327a4fddfa46aa7805974c8ea1e856
crsnyders <crsnyders@localhost>
2011-12-06 11:57:40 +0200

    sdst

---
+++ src/SDY1/SDY009.java
@@ -0,0 +1,22 @@
+package SDY1;
+import main.Card;
+import main.CardAttriubtes.Attribute;
+import main.CardAttriubtes.CardType;
+import main.CardAttriubtes.MonsterType;
+import main.CardAttriubtes.SubType;
+import main.table;
+public  class SDY009 extends Card
+{
+public SDY009()
+{
+ super("SDY","Celtic Guardian","SDY-009","/images/sdy/SDY-009.jpg",CardType.MONSTER,MonsterType.NORMAL,null,null,Attribute.EARTH,SubType.WARRIOR,4,1400,1200,"An elf who learned to wield a sword. He baffles enemies with lightning-swift attacks.");
+}  
+    public void mainEffect(table gameTable,int mainPlayer)
+    {}
+    @Override
+    public void secondEffect(table gameTable,int mainPlayer)
+    {}
+    @Override
+    public void triggerEffect(Card cardPlayed)
+    {}
+}
