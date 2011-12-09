2b4ec515eba4bd12eaf0303fcd9c601dd2c8e1ab
crsnyders <crsnyders@localhost>
2011-12-09 14:46:27 +0200

    changes

--- src/main/utils.java
+++ src/main/utils.java
@@ -2,10 +2,15 @@ package main;
 
 import java.util.Vector;
 
+import main.CardAttriubtes.CardType;
+import main.CardAttriubtes.SubType;
+
 public class utils
 {
 private static final int MONSTER = 1;
-private static final int SPELL = 0; 
+private static final int SPELL_TRAP = 0; 
+private static final int ATTACK = 0;
+private static final int DEFENCE =1;
 
 
 public static void toManyCards(int player)
@@ -92,18 +97,18 @@ public static void toManyCards(int player)
         int attack = table.monsterZone[attackingPlayer][attacker].currentCard.attack;
         if(defence > attack)
         {
-            dmgPlayer(attackingPlayer, defence-attack);
+            changePlayerLifePoints(attackingPlayer, defence-attack);
         }
         else if(defence < attack)
         {
             destroyCard(defender,defendingPlayer,MONSTER);
         }
     }
-    public static void destroyCard(int cardToDestroy,int player,int cardType)
+    private static void destroyCard(int cardToDestroy,int player,int cardType)
     {
         switch(cardType)
         {
-            case SPELL:
+            case SPELL_TRAP:
                 table.graveyard.get(player).add(table.spellZone[player][cardToDestroy].currentCard);
                 table.spellZone[player][cardToDestroy] = null;
                 break;
@@ -113,13 +118,74 @@ public static void toManyCards(int player)
                 break;
         }
     }
-    private static void dmgPlayer(int player, int amount)
+    public static void changePlayerLifePoints(int player, int amount)
     {
-        table.lifepoints[player]-=amount;
+        table.lifepoints[player]+=amount;
     }
     private static void showFaceDown(int feceDownCard)
     {
         
     }
+    public static void changeDefenceValue(int value, int numberOfTurns,int player)
+    {
+        //TODO ask to choose a monster
+        int zoneNumber =0;
+        table.monsterZone[player][zoneNumber].defenceValue+=value;
+        
+    }
+    public static void changeAttackValue(int value, int numberOfTurns,int player)
+    {
+        //TODO ask to choose a monster
+        int zoneNumber =0;
+        table.monsterZone[player][zoneNumber].defenceValue+=value;
+        
+    }
+    public static void destroyTrap()
+    {
+        //TODO wait for select
+       int traptoDestroy =0;
+       int player =0;
+       destroyCard(traptoDestroy, player, SPELL_TRAP);
+    }
+    public static void destroMonster(int player)
+    {
+        //TODO select monster
+        int monsterToDestroy = 0;
+        player=0;
+        destroyCard(monsterToDestroy, player, MONSTER);
+    }
+    private static void changeBySubType(SubType subType, int value, int atkOrDef)
+    {
+        for(int i =0;i<2;i++)
+        {
+            for(int p=0;p<5;p++)
+            {
+                if(table.monsterZone[i][p]!=null)
+                {
+                    if(table.monsterZone[i][p].currentCard.subType==subType)
+                    {
+                        switch(atkOrDef)
+                        {
+                        case ATTACK:
+                            table.monsterZone[i][p].attackValue+=value;
+                            break;
+                        case DEFENCE:
+                            table.monsterZone[i][p].defenceValue+=value;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    public static void changeAttackBySubType(SubType subType, int value)
+    {
+       changeBySubType(subType, value, ATTACK);
+    }
+    public static void changeDefenceBySubType(SubType subType, int value)
+    {
+        changeBySubType(subType, value, DEFENCE);
+    }
+   
             
 }
